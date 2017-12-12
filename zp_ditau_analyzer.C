8c0151c2f09810813f6dbf8739d3f345beab571b
athompson-tamu <31362409+athompson-tamu@users.noreply.github.com>
2017-12-12 16:57:43 -0600

    create zp analyzer

---
+++ zp_ditau_analyzer.C
@@ -0,0 +1,74 @@
+// An analyzer to produce the b-tagged jet / b-tagged + mu PT ratio.
+// USAGE:
+// gSystem->Load("<path_to_delphes>/Delphes/libDelphes.so");
+// .x PTRatioAnalyzer.C("input_file.root", "saved_histogram_name.root", "plot_name", nbins);
+
+#ifdef __CLING__
+R__LOAD_LIBRARY(libDelphes)
+#include "classes/DelphesClasses.h"
+#include "external/ExRootAnalysis/ExRootTreeReader.h"
+#include "external/ExRootAnalysis/ExRootResult.h"
+#else
+class ExRootTreeReader;
+class ExRootResult;
+#endif
+
+void PTRatioAnalyzer(const char *file_name, const char *save_name, const char *plot_name, int nbins$
+  gSystem->Load("/home/thompson/MG5_aMC_2_6_0/Delphes/libDelphes.so");
+
+  // Create a chain of root trees.
+  TChain chain("Delphes");
+  chain.Add(file_name);
+
+  // Create object of class ExRootTreeReader.
+  ExRootTreeReader *tree_reader = new ExRootTreeReader(&chain);
+  Long64_t number_of_entries = tree_reader->GetEntries();
+
+  // Get pointers to branches used in this analysis.
+  TClonesArray *branch_mu = tree_reader->UseBranch("Muon");
+  TClonesArray *branch_jet = tree_reader->UseBranch("Jet");
+  TClonesArray *branch_met = tree_reader->UseBranch("MissingET");
+
+  // Book histograms.
+  TH1F *hist_MET = new TH1F(plot_name + ": Normalized Missing ET, "", nbins, 0., 1.);
+  hist_MET->GetXaxis()->SetTitle("MET/Mmumu");
+  hist_MET->GetYaxis()->SetTitle("Nb events");
+
+  TH1F *hist_lj_mass = new TH1F(plot_name + ": M(lj)", "", nbins, 0., 1.);
+  hist_lj_mass->GetXaxis()->SetTitle("M(mu,j)");
+  hist_lj_mass->GetYaxis()->SetTitle("Nb events");
+
+  TH1F *hist_HT_LT = new TH1F(plot_name + ": HT - LT", "", nbins, 0., 1.);
+  hist_ht_lt->GetXaxis()->SetTitle("HT - LT");
+  hist_ht_lt->GetYaxis()->SetTitle("Nb events");
+
+  TH1F *hist_zeta = new TH1F(plot_name + ": Zeta", "", nbins, 0., 1.);
+  hist_zeta->GetXaxis()->SetTitle("zeta");
+  hist_zeta->GetYaxis()->SetTitle("Nb events");
+
+  // Event loop.
+  for(Int_t entry = 0; entry < number_of_entries; ++entry) {
+
+    tree_reader->ReadEntry(entry);
+
+    // Declare physics objects.
+    Muon *muon;
+    Jet *jet;
+    MissingET *met;
+
+    // Declare running indices.
+    Int_t muon_n = branch_mu->GetEntries();
+    Int_t jet_n = branch_jet->GetEntries();
+    Int_t met_n = branch_met->GetEntries();
+
+  }
+
+  // Draw hist and save.
+  TCanvas *c = new TCanvas();
+
+  hist_lj_mass->Scale(1/hist_lj_mass->Integral());
+  hist_lj_mass->Draw();
+
+  TFile *f = new TFile(save_name,"RECREATE");
+  hist_lj_mass->Write("",TObject::kOverwrite);
+}
