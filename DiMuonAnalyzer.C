d7e1769f970993952770b2951f06290742c25e0e
Adrian Thompson <31362409+athompson-tamu@users.noreply.github.com>
2018-01-20 14:04:52 -0600

    pushing brazos version

--- DiMuonAnalyzer.C
+++ DiMuonAnalyzer.C
@@ -1,31 +1,56 @@
 // An analyzer to produce key selection criteria plots for dimuon processes.
-// EXAMPLE USAGE:
+// Run with:
 // gSystem->Load("<path_to_delphes>/Delphes/libDelphes.so");
-// .x DiMuonAnalyzer.C("input_file.root", "Zprime (500 GeV)", nbins);
 
 #ifdef __CLING__
 #include "classes/DelphesClasses.h"
 #include "external/ExRootAnalysis/ExRootTreeReader.h"
 #include "external/ExRootAnalysis/ExRootResult.h"
-#include "lester_mt2_bisect.h"
 #else
 class ExRootTreeReader;
 class ExRootResult;
 #endif
 
-// Define global constants.
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <lester_mt2_bisect.h>
+
 Double_t kMuonMass = 0.105658389;  // GeV
-Double_t previous_mt2 = 999.0;
-Int_t accepted_events = 0;
+Int_t count_pre = 0;
+Int_t count_top_bound = 0;
+Int_t count_met = 0;
+Int_t count_ht_lt = 0;
+Int_t count_UnbMT2 = 0;
 Int_t event_count = 0;
 
-void DiMuonAnalyzer(const char *file_name, const char *sample_desc, int nbins) {
+void DiMuonAnalyzer(const char *sample_desc, int nbins) {
   gSystem->Load("libDelphes.so");
 
   // Create a chain of root trees.
   TChain chain("Delphes");
-  chain.Add(file_name);
 
+  // ttbar samples.
+  chain.Add("/fdata/scratch/mexanick/ttbar/tt_b.root");
+  chain.Add("/fdata/scratch/mexanick/ttbar/tt_c.root");
+  chain.Add("/fdata/scratch/mexanick/ttbar/tt_g.root");
+  chain.Add("/fdata/scratch/mexanick/ttbar/tt_j.root");
+  chain.Add("/fdata/scratch/mexanick/ttbar/tt_k.root");
+  chain.Add("/fdata/scratch/mexanick/ttbar/tt_l.root");
+
+  // Z' (500 GeV) samples.
+/*  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs1/Events/run_05/tag_2_delphes_events.root");
+  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs1/Events/run_06/tag_2_delphes_events.root");
+  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs1/Events/run_07/tag_2_delphes_events.root");
+  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs1/Events/run_08/tag_1_delphes_events.root");
+  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs2/Events/run_05/tag_2_delphes_events.root");
+  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs2/Events/run_06/tag_2_delphes_events.root");
+  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs2/Events/run_07/tag_2_delphes_events.root");
+  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs2/Events/run_08/tag_1_delphes_events.root");
+  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs5/Events/run_01/tag_6_delphes_events.root");
+  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs5/Events/run_02/tag_5_delphes_events.root");
+  chain.Add("/fdata/scratch/mexanick/z_prime_500GeV_cs5/Events/run_03/tag_5_delphes_events.root");
+*/
   // Create object of class ExRootTreeReader.
   ExRootTreeReader *tree_reader = new ExRootTreeReader(&chain);
   Long64_t number_of_entries = tree_reader->GetEntries();
@@ -37,40 +62,25 @@ void DiMuonAnalyzer(const char *file_name, const char *sample_desc, int nbins) {
 
   // Book histograms.
   TH1F *hist_pair_mass = new TH1F("pair_mass", "Max{M(j,mu)}", nbins, 0., 300.);
-  hist_pair_mass->GetXaxis()->SetTitle("M(mu,j)");
-  hist_pair_mass->GetYaxis()->SetTitle("a.u.");
-
   TH1F *hist_MET = new TH1F("met", "Normalized Missing ET", nbins, 0., 1.);
-  hist_MET->GetXaxis()->SetTitle("MET/Mmumu");
-  hist_MET->GetYaxis()->SetTitle("a.u.");
-
   TH1F *hist_HT_LT = new TH1F("HT_LT", "HT - LT", nbins, -500., 500.);
-  hist_HT_LT->GetXaxis()->SetTitle("HT - LT");
-  hist_HT_LT->GetYaxis()->SetTitle("a.u.");
-
   TH1F *hist_MT2 = new TH1F("MT2", "MT2", nbins, 0., 1.);
-  hist_MT2->GetXaxis()->SetTitle("M_{T2}");
-  hist_MT2->GetYaxis()->SetTitle("a.u.");
-
-  TH2F *hist_MT2_dPhi = new TH2F("mt2_vs_dPhi", "Max #delta #phi vs. M_{T2}", nbins,
-                                 0., 10., nbins, 0., 3.14);
-  hist_MT2_dPhi->GetXaxis()->SetTitle("M_{T2}");
-  hist_MT2_dPhi->GetYaxis()->SetTitle("max[#delta#phi(#mu^{+}+, MET), #delta#phi(#mu^{-}, MET)]");
-
-  TH1F *hist_pt_mu = new TH1F("mupt", "", nbins, 0., 300.);
-
-  TH2F *hist_topology = new TH2F("topo", "Zprime(dimuon) Topology", nbins,
-                                 0., 50., nbins, -3.14, 3.14);
-  hist_topology->GetXaxis()->SetTitle("M_{T2}");
-  hist_topology->GetYaxis()->SetTitle("max[#delta#phi] - #delta#phi(#mu^{+}, #mu^{-})");
-
   TH1F *hist_unboosted_MT2 = new TH1F("unboost_mt2", "", nbins, 0., 40.);
 
   // Event loop.
   for(Int_t entry = 0; entry < number_of_entries; ++entry) {
 
     tree_reader->ReadEntry(entry);
+
     event_count++;
+    if (event_count % 10000 == 0) {
+      printf("Processed %d / %lld events \n", event_count, number_of_entries);
+      printf("Preselection: %d / %d events \n", count_pre, event_count);
+      printf("Top Mass Bound: %d / %d events \n", count_top_bound, event_count);
+      printf("Normalized MET: %d / %d events \n", count_met, event_count);
+      printf("HT - LT: %d / %d events \n", count_ht_lt, event_count);
+      printf("Unboosted MT2: %d / %d events \n", count_UnbMT2, event_count);
+    }
 
     // Declare physics objects.
     Muon *muon;
@@ -81,35 +91,35 @@ void DiMuonAnalyzer(const char *file_name, const char *sample_desc, int nbins) {
     TLorentzVector mu_plus;
     TLorentzVector mu_minus;
 
-    // Declare running indices.
     Int_t jet_size = branch_jet->GetEntries();
     Int_t met_size = branch_met->GetEntries();
     Int_t muon_size = branch_muon->GetEntries();
 
-    // Redeclare preselection states.
     if (jet_size < 2) continue;
     bool found_btag = false;
+    bool found_jet = false;
     bool found_mu_plus = false;
     bool found_mu_minus = false;
 
     // Find the leading OS Muon pair.
     for (Int_t ii = 0; ii < muon_size; ii++) {
       muon = (Muon*) branch_muon->At(ii);
+      // Looking for highest-pT mu+.
       if (muon->Charge == 1 && muon->PT > mu_plus.Pt()) {
         mu_plus.SetPtEtaPhiM(muon->PT, muon->Eta, muon->Phi, kMuonMass);
         found_mu_plus = true;
       }
+      // Looking for highest-pT mu-.
       if (muon->Charge == -1 && muon->PT > mu_minus.Pt()) {
         mu_minus.SetPtEtaPhiM(muon->PT, muon->Eta, muon->Phi, kMuonMass);
         found_mu_minus = true;
       }
     }
 
-    // Loop over jets and find the highest pt BTag jet.
+    // Loop over jets and find the highest-pT BTag jet.
     Int_t leading_btag_id = -1;
     for (Int_t ii = 0; ii < jet_size; ii++) {
       jet = (Jet*) branch_jet->At(ii);
-      // Find the leading b-tagged jet.
       if (jet->BTag == 1 && jet->PT > btag_jet.Pt()) {
         btag_jet.SetPtEtaPhiM(jet->PT, jet->Eta, jet->Phi, jet->Mass);
         leading_btag_id = ii;
@@ -117,20 +127,20 @@ void DiMuonAnalyzer(const char *file_name, const char *sample_desc, int nbins) {
       }
     }
 
-    // Find the other highest-pt jet with ID different than the leading BTag.
+    // Find the other highest-pT jet with ID different than the leading BTag.
     for (Int_t ii = 0; ii < jet_size; ii++) {
       jet = (Jet*) branch_jet->At(ii);
+      // Must not be a Tau jet with ID different than the leading b-tag.
       if (jet->TauTag == 0 && jet->PT > second_jet.Pt() && ii != leading_btag_id) {
         second_jet.SetPtEtaPhiM(jet->PT, jet->Eta, jet->Phi, jet->Mass);
+        found_jet = true;
       }
     }
-
     // Place preselection cuts.
-    if (!found_btag) continue;
+    if (!found_btag || !found_jet) continue;
     if (!found_mu_plus || !found_mu_minus) continue;
     if (btag_jet.Pt() < 30.) continue;
     if (second_jet.Pt() < 30.) continue;
-    if (abs(mu_plus.Eta()) > 2.4 || abs(mu_minus.Eta()) > 2.4) continue;
     if (abs(btag_jet.Eta()) > 2.4 || abs(second_jet.Eta()) > 2.4) continue;
 
     // Now that we have ID'd our 4 particles, construct more 4-vectors.
@@ -145,53 +155,50 @@ void DiMuonAnalyzer(const char *file_name, const char *sample_desc, int nbins) {
 
     // Fill histograms.
 
-    hist_pt_mu->Fill(mu_plus.Pt());
-
-    // Calculate and fill muon/jet pair mass.
-    Double_t choice_pair_mass;
+    // (1) Calculate and fill muon/jet pair mass.
+    Double_t top_mass_bound;
+    // If (mu+, b) - (mu-, j) has the smallest mass difference...
     if (abs(mu_p_b.M() - mu_m_j.M()) < abs(mu_p_j.M() - mu_m_b.M())) {
-      choice_pair_mass = std::max(mu_p_b.M(), mu_m_j.M());
-    } else {
-      choice_pair_mass = std::max(mu_p_j.M(), mu_m_b.M());
+      top_mass_bound = std::max(mu_p_b.M(), mu_m_j.M()); // ... take the max pair.
+    } else { // otherwise, take the max of the other set of pairs.
+      top_mass_bound = std::max(mu_p_j.M(), mu_m_b.M());
     }
-    hist_pair_mass->Fill(choice_pair_mass);
+    hist_pair_mass->Fill(top_mass_bound);
 
-    // Calculate and fill MET for the event.
+    // (2) Calculate and fill MET for the event.
     met = (MissingET*) branch_met->At(0);
-    Double_t normalized_met = (met->MET / dimuon.M());
+    Double_t normalized_met = (met->MET) / (dimuon.M());
     hist_MET->Fill(normalized_met);
 
-    // Calculate MT2 (http://www.hep.phy.cam.ac.uk/~lester/mt2/).
-    met_P4.SetPtEtaPhiE(met->MET, 0, met->Phi, met->MET);
-    Double_t mVisA = mu_plus.M(); // Mass of visible object on side A.
-    Double_t pxA = mu_plus.Px(); // x momentum of visible object on side A.
-    Double_t pyA = mu_plus.Py(); // y momentum of visible object on side A.
+    // (3) Calculate and fill HT - LT.
+    Double_t H_T = abs(btag_jet.Pt()) + abs(second_jet.Pt());
+    Double_t L_T = abs(mu_plus.Pt()) + abs(mu_minus.Pt());
+    hist_HT_LT->Fill(H_T - L_T);
 
-    Double_t mVisB = mu_minus.M(); // Mass of visible object on side B.
-    Double_t pxB = mu_minus.Px(); // x momentum of visible object on side B.
-    Double_t pyB = mu_minus.Py(); // y momentum of visible object on side B.
+    // (4) Calculate MT2 (http://www.hep.phy.cam.ac.uk/~lester/mt2/).
+    met_P4.SetPtEtaPhiM(met->MET, met->Eta, met->Phi, 0);
+    double mVisA = kMuonMass; // Mass of visible object on side A.
+    double pxA = mu_plus.Px(); // x momentum of visible object on side A.
+    double pyA = mu_plus.Py(); // y momentum of visible object on side A.
 
-    Double_t pxMiss = met_P4.Px(); // x component of missing transverse momentum.
-    Double_t pyMiss = met_P4.Py(); // y component of missing transverse momentum.
+    double mVisB = kMuonMass; // Mass of visible object on side B.
+    double pxB = mu_minus.Px(); // x momentum of visible object on side B.
+    double pyB = mu_minus.Py(); // y momentum of visible object on side B.
 
-    Double_t chiA = 0.0; // Hypothesised mass of invisible on side A.
-    Double_t chiB = 0.0; // Hypothesised mass of invisible on side B.
+    double pxMiss = met_P4.Px(); // x component of missing transverse momentum.
+    double pyMiss = met_P4.Py(); // y component of missing transverse momentum.
 
-    Double_t desiredPrecisionOnMt2 = 0; // Algo aims for machine precision.
+    double chiA = 0.0; // Hypothesised mass of invisible on side A.
+    double chiB = 0.0; // Hypothesised mass of invisible on side B.
 
-    Double_t MT2 = asymm_mt2_lester_bisect::get_mT2(
-                   mVisA, pxA, pyA,
-                   mVisB, pxB, pyB,
-                   pxMiss, pyMiss,
-                   chiA, chiB,
-                   desiredPrecisionOnMt2);
+    double desiredPrecisionOnMt2 = 0.0; // Algo aims for machine precision.
+
+    double MT2 = asymm_mt2_lester_bisect::get_mT2(mVisA, pxA, pyA,
+                                                mVisB, pxB, pyB,
+                                                pxMiss, pyMiss,
+                                                chiA, chiB,
+                                                desiredPrecisionOnMt2);
     hist_MT2->Fill(MT2);
-
-    // Report lowest MT2 value.
-    if (MT2 < previous_mt2) {
-      previous_mt2 = MT2;
-      printf("%f \n", MT2);
-    }
 
     // UNBOOSTED system.
     // First calculate recoil in the transverse plane.
@@ -200,103 +207,65 @@ void DiMuonAnalyzer(const char *file_name, const char *sample_desc, int nbins) {
     TLorentzVector unboosted_mu_minus = jet_recoil + mu_minus;
     TLorentzVector unboosted_met = jet_recoil + met_P4;
 
-    // Calculate UNBOOSTED MT2.
-    Double_t unboost_pxA = unboosted_mu_plus.Px();
-    Double_t unboost_pyA = unboosted_mu_plus.Py();
+    // (5) Calculate UNBOOSTED MT2.
+    double unboost_pxA = unboosted_mu_plus.Px();
+    double unboost_pyA = unboosted_mu_plus.Py();
 
-    Double_t unboost_pxB = unboosted_mu_minus.Px();
-    Double_t unboost_pyB = unboosted_mu_minus.Py();
-    Double_t unboost_pxMiss = unboosted_met.Px();
-    Double_t unboost_pyMiss = unboosted_met.Py();
+    double unboost_pxB = unboosted_mu_minus.Px();
+    double unboost_pyB = unboosted_mu_minus.Py();
+    double unboost_pxMiss = unboosted_met.Px();
+    double unboost_pyMiss = unboosted_met.Py();
 
-    Double_t unboosted_MT2 = asymm_mt2_lester_bisect::get_mT2(
+    double unboosted_MT2 = asymm_mt2_lester_bisect::get_mT2(
                              mVisA, unboost_pxA, unboost_pyA,
                              mVisB, unboost_pxB, unboost_pyB,
                              unboost_pxMiss, unboost_pyMiss, chiA, chiB,
                              desiredPrecisionOnMt2);
     hist_unboosted_MT2->Fill(unboosted_MT2);
 
-    // Calculate and fill HT - LT.
-    Double_t H_T = abs(btag_jet.Pt()) + abs(second_jet.Pt());
-    Double_t L_T = abs(mu_plus.Pt()) + abs(mu_minus.Pt());
-    hist_HT_LT->Fill(H_T - L_T);
-
-    // Calculate dphi/mt2
-    Double_t max_dphi = std::max(abs(mu_plus.DeltaPhi(met_P4)),
-                                 abs(mu_minus.DeltaPhi(met_P4)));
-
-    hist_MT2_dPhi->Fill(MT2, max_dphi);
-
-    // Calculate max{dPhi(MET, mu)} - dPhi(mu,mu)
-    // positive --> Topo 1
-    // negative --> Topo 2 (trivial zero)
-    Double_t dphi_muons = abs(mu_plus.DeltaPhi(mu_minus));
-    hist_topology->Fill(MT2, max_dphi - dphi_muons);
-
 
     // Calculate things for acceptance.
-    Double_t MT2uM = (MT2 - unboosted_MT2) / dimuon.M();
-//    if (unboosted_MT2 > 10.0) continue;
-//    if (H_T - L_T > 0) continue;
-//    if (normalized_met > 0.2) continue;
-//    if (choice_pair_mass < 170) continue;
-
-    accepted_events++;
+    count_pre++;
+    if (top_mass_bound > 170) {
+      count_top_bound++;
+      if (normalized_met < 0.2) {
+        count_met++;
+        if (H_T - L_T < 0) {
+          count_ht_lt++;
+        }
+      }
+    }
 
   } // End event loop.
 
-  printf("Accepted %d / %d events \n", accepted_events, event_count);
+  printf("Preselection: %d / %d events \n", count_pre, event_count);
+  printf("Top Mass Bound: %d / %d events \n", count_top_bound, event_count);
+  printf("Normalized MET: %d / %d events \n", count_met, event_count);
+  printf("HT - LT: %d / %d events \n", count_ht_lt, event_count);
+  printf("Unboosted MT2: %d / %d events \n", count_UnbMT2, event_count);
+
+
 
   // Draw histograms and save them in a .root format.
-  TCanvas *c1 = new TCanvas();
   hist_pair_mass->Scale(1/hist_pair_mass->Integral());
-  hist_pair_mass->Draw("HIST");
-
-  TCanvas *c2 = new TCanvas();
   hist_MET->Scale(1/hist_MET->Integral());
-  hist_MET->Draw("HIST");
-
-  TCanvas *c3 = new TCanvas();
   hist_HT_LT->Scale(1/hist_HT_LT->Integral());
-  hist_HT_LT->Draw("HIST");
-
-  TCanvas *c4 = new TCanvas();
   hist_MT2->Scale(1/hist_MT2->Integral());
-  hist_MT2->Draw("HIST");
-
-  gStyle->SetOptStat(0);
-  TCanvas *c5 = new TCanvas();
-  hist_MT2_dPhi->Draw("COL2Z");
-
-  TCanvas *c6 = new TCanvas();
-  hist_pt_mu->Scale(1/hist_pt_mu->Integral());
-  hist_pt_mu->Draw("HIST");
-
-  TCanvas *c7 = new TCanvas();
-  hist_topology->Draw("COL2Z");
-
   hist_unboosted_MT2->Scale(1/hist_unboosted_MT2->Integral());
 
-  std::string mass_name = string(sample_desc) + " max(M(#mu,j))";
-  std::string met_name = string(sample_desc) + " MET";
-  std::string HT_LT_name = string(sample_desc) + " H_{T} - L_{T}";
-  std::string mt2_name = string(sample_desc) + " MT2";
-
   TFile *f1 = new TFile("hist_dimuon_mujet_mass.root", "UPDATE");
-  hist_pair_mass->Write(mass_name.c_str(), TObject::kOverwrite);
+  hist_pair_mass->Write(sample_desc, TObject::kOverwrite);
 
   TFile *f2 = new TFile("hist_dimuon_MET.root", "UPDATE");
-  hist_MET->Write(met_name.c_str(), TObject::kOverwrite);
+  hist_MET->Write(sample_desc, TObject::kOverwrite);
 
   TFile *f3 = new TFile("hist_dimuon_HT_LT.root", "UPDATE");
-  hist_HT_LT->Write(HT_LT_name.c_str(), TObject::kOverwrite);
+  hist_HT_LT->Write(sample_desc, TObject::kOverwrite);
 
   TFile *f4 = new TFile("hist_dimuon_MT2.root" ,"UPDATE");
-  hist_MT2->Write(mt2_name.c_str(), TObject::kOverwrite);
+  hist_MT2->Write(sample_desc, TObject::kOverwrite);
 
-  TFile *f5 = new TFile("hist_dimuon_ptmu.root", "UPDATE");
-  hist_pt_mu->Write(sample_desc, TObject::kOverwrite);
-
-  TFile *f17 = new TFile("hist_dimuon_unboost_MT2.root", "UPDATE");
+  TFile *f5 = new TFile("hist_dimuon_unboost_MT2.root", "UPDATE");
   hist_unboosted_MT2->Write(sample_desc, TObject::kOverwrite);
+
 }
